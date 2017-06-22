7b2f4ed85f082afc6b2707b2e0061967ecc28e85
nyiss <tymi223@gmail.com>
2017-06-23 00:57:54 +0200

    nearly done
    Mailer done, needs jquery to stop from redirecting to blank page + website response after sending the form through

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,31 +1,40 @@
 <?php
+require 'phpmailer/PHPMailerAutoload.php';
 
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $body = $_POST['body'];
-      $to = 'asd'
+$name = $_POST['name'];
+$email = $_POST['email'];
+$body = $_POST['body'];
 
-      require 'phpmailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
 
-      $mail = new PHPMailer();
-      $mail->IsSmtp();
-      $mail->SMTPDebug = 1;
-      $mail->SMTPAuth = true;
-      $mail->SMTPSecure = 'ssl';
-      $mail->Host = "smtp.gmail.com";
-      $mail->Port = 465;
-      $mail->isHTML(false);
-      $mail->Username = "asd";
-      $mail->Password = "asd";
-      $mail->setFrom("asd");
-      $mail->Subject = 'Formularz kontaktowy - ogrodyaurora.pl'
-      $mail->Body = $body;
-      $mail->AddAddress($to);
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-      if (!$mail->Send()) {
-        echo "Błąd przy wysyłaniu";
-      }else{
-        echo "Wysłano";
-      }
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'ogrodyaurora@gmail.com';                 // SMTP username
+$mail->Password = 'Ogrody123';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
 
+$mail->setFrom('ogrodyaurora@gmail.com', 'Konto obslugujace formularz kontaktowy');
+$mail->addAddress('tymi223@gmail.com');     // Add a recipient
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Formularz kontaktowy - ogrodyaurora.pl';
+$mail->Body    = '<html>
+                    <body>
+                    <p>Imie i nazwisko : '.$name.'<br></p>
+                    <p>Email : '.$email.'<br></p>
+                    Napisał : <br><br>'.$body.'<br>
+                    </body>
+                  </html>';
+$mail->AltBody = 'Imie i nazwisko : '.$name.'Email : '.$email.'Napisał : '.$body;
+
+if(!$mail->send()) {
+  echo "<script>window.alert('Nie udało wysłać się wiadomości. <br>Prosimy o bezpośredni kontakt mailowy lub telefoniczny.')</script>";
+} else {
+  echo "<script>window.alert('Wiadomość wysłana pomyślnie!')</script>";
+}
 ?>
